a34383340d52eca597077ab1d1150512c98ddbad
Moustro <amaurymoustro@gmail.com>
2020-11-29 10:17:20 -0600

    AppCalculadoraCambio

--- AppCalculadora/app/src/main/java/com/example/appcalculadora/MainActivity.java
+++ AppCalculadora/app/src/main/java/com/example/appcalculadora/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.appcalculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), "Bienvenido", Toast.LENGTH_SHORT).show();
+                Intent cambio = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(cambio);
+                finish();
+            }
+        }, 5000);
     }
 }
